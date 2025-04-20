7a2764319a9f5d37e5edaa4f9f397eb83abeda79
HSolaire <2876798934@qq.com>
2025-04-20 23:10:30 +0800

    修改基础配置，第一次提交

---
+++ src/main/java/com/hang/hoj/constant/FileConstant.java
@@ -0,0 +1,14 @@
+package com.hang.hoj.constant;
+
+/**
+ * 文件常量
+ *
+ */
+public interface FileConstant {
+
+    /**
+     * COS 访问地址
+     * todo 需替换配置
+     */
+    String COS_HOST = "https://hang.icu";
+}
---
+++ src/main/java/com/hang/hoj/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.hang.hoj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hang.hoj.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
